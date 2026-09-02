7ac2e3b5cdaada42171c3f9c0b2075cd61289fdb
YashRaj9211 <yashraj.2500504.aesl@gmail.com>
2026-09-02 20:46:31 +0530

    pm2-config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "readkar-backend-api",
       script: ".venv/Scripts/uvicorn.exe",
-      args: "app.main:app --host 0.0.0.0",
+      args: "app.main:app --host 0.0.0.0 --port 8000 --workers 2",
       cwd: "./",
       interpreter: "none",
       watch: false,
