557de7dd5ed804f16994655d9ba5c94066a0ede1
YashRaj9211 <yashraj.2500504.aesl@gmail.com>
2026-09-02 19:01:58 +0530

    fixed pm2 run script

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   apps: [
     {
-      name: "pdf-backend-api",
+      name: "readkar-backend-api",
       script: ".venv/Scripts/uvicorn.exe",
-      args: "app.main:app --host 0.0.0.0 --port 8000 --reload",
+      args: "app.main:app --host 0.0.0.0",
       cwd: "./",
       interpreter: "none",
       watch: false,
